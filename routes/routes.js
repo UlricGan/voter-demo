3b3733919173ee58b1b42a4eb5dbc3122da53828
ulric <hongxiang0310@gmail.com>
2014-03-22 00:26:23 +0800

    调整middleware位置

--- routes/routes.js
+++ routes/routes.js
@@ -2,21 +2,14 @@ var result=require('../app/controllers/result');
 var votes=require('../app/controllers/votes');
 
 
-module.exports=function(app, emitter){
+module.exports=function(app){
 
-	var setEmitter=function(req, res, next){
-		if(!req.emitter){
-			req.emitter=emitter;
-		}
 
-		next();
-	};
+	app.get('/', result.index);
+	app.get('/vote', votes.index);
 
-	app.get('/', setEmitter, result.index);
-	app.get('/vote', setEmitter, votes.index);
-
-	app.get('/roll/:aspect', setEmitter, result.rollResult);
-	app.get('/vote/:aspect/:competitor', setEmitter, votes.updateVote);
+	app.get('/roll/:aspect', result.rollResult);
+	app.get('/vote/:aspect/:competitor', votes.updateVote);
 
 
 
--- server.js
+++ server.js
@@ -21,6 +21,15 @@ var app=express();
 var server=http.createServer(app);
 var io=socket.listen(server);
 
+var setEmitter=function(emitter){
+	return function(req, res, next){
+		if(!req.emitter){
+			req.emitter=emitter;
+		}
+		next();
+	};
+};
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'app/views'));
@@ -28,6 +37,7 @@ app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(setEmitter(emitter));
 app.use(express.cookieParser());
 app.use(express.session({
 	secret: 'myvote',
@@ -45,7 +55,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-routes(app, emitter);
+routes(app);
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
